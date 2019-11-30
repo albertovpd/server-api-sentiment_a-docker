54f43817b3f43628c1f333b6d7e912fea5da82bf
albertovpd <a.vargas.pina@gmail.com>
2019-11-30 20:22:13 +0100

    my .py api works again

--- remote_server/my-application/mongodb/api.py
+++ remote_server/my-application/mongodb/api.py
@@ -5,54 +5,39 @@ from populate import db,coll
 import json
 from bson.json_util import dumps
 
+from populate import db,coll
+import json
+from bson.json_util import dumps
 
 
+@get("/chat/<chat_id>/list")
+def getChat(chat_id):
+    '''
+    This function admit onlu integers (Alex rules)
+    '''
+    db, coll = connectCollection('chats','messages')
+    db, collchat = connectCollection('chats','chats')
+    chatid_data = list(collchat.find({'Chat_id':int(chat_id)}))
+    chat_obj_id = chatid_data[0].get('_id')
+    chat_data = list(coll.find({'idChat':chat_obj_id}))
+    ret = {}
+    for i in range(len(chat_data)):
+        name = chat_data[i].get('userName')
+        message = chat_data[i].get('text')
+        date = chat_data[i].get('datetime')
+        ret[f'{name}_{date}'] = message        
+    return ret
+
+run(host='0.0.0.0', port=8080)
+
+
+'''@get("/")
+def index():
+    return dumps(coll.find())
+run(host="localhost", port=8080)'''
+
+'''
 @get("/")
 def index():
     return dumps(coll.find())
-run(host="localhost", port=8080)
-
-
-    
-
-'''from bottle import route, run, get, post, request
-import random
-from mongo import CollConection
-import bson
-
-@get("/")
-def index():
-    return {
-        "nombre": random.choice(["Pepe", "Juan", "Fran", "Luis"])
-    }
-
-
-@get("/chiste/<tipo>")
-def demo2(tipo):
-    print(f"un chiste de {tipo}")
-    if tipo == "chiquito":
-        return {
-            "chiste": "Van dos soldados en una moto y no se cae ninguno porque van soldados"
-        }
-    elif tipo == "eugenio":
-        return {
-            "chiste": "Saben aquell que diu...."
-        }
-    else:
-        return {
-            "chiste": "No puedorrr!!"
-        }
-
-@post('/add')
-def add():
-    print(dict(request.forms))
-    autor=request.forms.get("autor")
-    chiste=request.forms.get("chiste")  
-    return {
-        "inserted_doc": str(coll.addChiste(autor,chiste))}
-
-
-coll=CollConection('project2911','chats_sentiment_analysis')
-run(host='0.0.0.0', port=8080)
-
-'''
+run(host="localhost", port=8080)'''    
